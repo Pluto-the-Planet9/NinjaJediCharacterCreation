8fd522ed8996bfeb983f79498d992839c0891d94
Senior <Senior@G-02-05>
2024-09-11 13:04:22 -0400

    Add project files.

---
+++ Ninja Jedi/Character .cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninja_Jedi
+{
+    public class Character
+    {
+
+        private int health;
+        private int stregth;
+
+
+        public string Faction { get; set; }
+
+        public int Health 
+        { 
+            get {  return health; } 
+            set
+            {
+                if (value < 1)
+                    health = 1;
+                else if (value > 100)
+                    health = 100;
+                else
+                    health = value;
+            }
+        }
+
+        public int Stregth
+        {
+            get { return stregth; }
+            set
+            {
+                if (value < 1)
+                    stregth = 1;
+                else if (value > 100)
+                    stregth = 100;
+                else
+                    stregth = value;
+            }
+        }
+
+        public string SpecialSkill { get; set; }
+
+
+        public Character(string faction, int health, int stregth, string specialSkill)
+        {
+            Faction = faction;
+            Health = health;
+            Stregth = stregth;
+            SpecialSkill = specialSkill;
+        }
+
+
+        public void DisplayCharactorInfo() 
+        {
+            Console.WriteLine($"Faction: {Faction}");
+            Console.WriteLine($"Health: {Health}");
+            Console.WriteLine($"Stregth: {Stregth}");
+            Console.WriteLine($"Special Skill: {SpecialSkill}");
+        }
+
+        public void UseSpecialSkill()
+        {
+            Console.WriteLine($"{Faction} uses {SpecialSkill}!");
+        }
+
+    }
+}
---
+++ Ninja Jedi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninja_Jedi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Choose your faction (Jedi/Ninja): ");
+            string faction = Console.ReadLine();
+
+
+            Console.WriteLine("Enter Health points (1-100): ");
+            int health = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter stranth points (1-100): ");
+            int stranth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose your special skill (Force Push/Stealth Attack): ");
+            string specialSkill = Console.ReadLine();
+
+
+            Character playerCharactor = new Character(faction, health, stranth, specialSkill);
+
+
+            Console.WriteLine("\nCharactor Made Successfully");
+            playerCharactor.DisplayCharactorInfo();
+
+            Console.WriteLine("\nDo you want to use your special skill? (yes/no)");
+            string useSkill = Console.ReadLine().ToLower();
+
+            if (useSkill == "yes")
+            {
+                playerCharactor.UseSpecialSkill();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
